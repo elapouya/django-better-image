0170d3a8e244e87aea65b50d121f996b621cdb7b
elapouya <elapouya@gmail.com>
2019-08-14 14:56:45 +0200

    add main files

--- django_better_image/__init__.py
+++ django_better_image/__init__.py
@@ -1,4 +1,4 @@
-__version__='0.0.2'
+__version__='0.0.3'
 __author__ = "Eric Lapouyade"
 __copyright__ = "Copyright 2019, The Django Better Image Project"
 __credits__ = ["Eric Lapouyade"]
---
+++ scripts/make_statics.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# To be run *in* scripts dir
+# uses yui-compressor, closure compiler and sass :
+# download compiler .jar here : https://github.com/google/closure-compiler
+# sudo apt install yui-compressor
+# sudo apt-get install ruby-full build-essential rubygems
+# sudo gem install sass
+# if needed :
+# sudo gem install rb-inotify
+
+BETTERIMAGEDIR=$(dirname $0)/../django_better_image
+CSSDIR=$BETTERIMAGEDIR/static/css
+JSDIR=$BETTERIMAGEDIR/static/js
+
+BETTERIMAGE_SCSS=$CSSDIR/better_image.scss
+BETTERIMAGE_CSS=$CSSDIR/better_image.css
+BETTERIMAGE_MIN_CSS=$CSSDIR/better_image.min.css
+BETTERIMAGE_JS=$JSDIR/better_image.js
+BETTERIMAGE_MIN_JS=$JSDIR/better_image.min.js
+
+JS_COMPRESS="java -jar $HOME/bin/closure-compiler.jar --language_out=ECMASCRIPT_2015"
+YUI_COMPRESS=yui-compressor
+SAAS=/usr/local/bin/sass
+
+if [[ -d $CSSDIR ]]
+then
+    set -x
+    $SAAS $BETTERIMAGE_SCSS $BETTERIMAGE_CSS
+    $YUI_COMPRESS $BETTERIMAGE_CSS -o $BETTERIMAGE_MIN_CSS
+    $YUI_COMPRESS $CSSDIR/better_image_fonticons.css -o $CSSDIR/better_image_fonticons.min.css
+    set +x
+fi
+
+if [[ -d $JSDIR ]]
+then
+    set -x
+    rm -f $JSDIR/better_image.min.js
+    $JS_COMPRESS $JSDIR/better_image.js > $JSDIR/better_image.min.js
+    set +x
+fi
